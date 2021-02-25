d9c8a82c20e20e3493dd7a25528fecbd5a4f87ba
Victoria Ishnyakova <ishnyakova.vika@yandex.ru>
2021-02-25 18:13:10 +0700

    added app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthorizationComponent } from './components/authorization/authorization.component';
+import { MeteringDevicesComponent } from './components/metering-devices/metering-devices.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'authorization', component: AuthorizationComponent },
+  { path: 'metering-devices', component: MeteringDevicesComponent },
+  { path: '',   redirectTo: '/authorization', pathMatch: 'full' },
+  { path: '**', redirectTo: '/authorization' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
