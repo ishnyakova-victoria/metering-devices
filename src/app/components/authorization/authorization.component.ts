1a09f96a359396e3ea7de8c1b869a3288ea98dff
Victoria Ishnyakova <ishnyakova.vika@yandex.ru>
2021-02-26 23:45:38 +0700

    added log in

--- src/app/components/authorization/authorization.component.ts
+++ src/app/components/authorization/authorization.component.ts
@@ -1,16 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AuthorizationService } from '../../providers/authorization.service';
 
 @Component({
   selector: 'app-authorization',
   templateUrl: './authorization.component.html',
   styleUrls: ['./authorization.component.less']
 })
-export class AuthorizationComponent implements OnInit {
+export class AuthorizationComponent implements OnInit, OnDestroy {
+  public authorizationForm: FormGroup = new FormGroup({        
+    "email": new FormControl('', [
+      Validators.required, 
+      Validators.email
+    ]),
+    "password": new FormControl('', Validators.required)
+  });
   public hidePassword: boolean = true;
+  public logInError: string = null;
 
-  constructor() { }
+  private _subscription: Array<Subscription> = [];
 
-  ngOnInit(): void {
+  constructor(
+    private _router: Router,
+    private _authorizationService: AuthorizationService
+  ) { }
+
+  ngOnInit(): void { }
+
+  ngOnDestroy(): void {
+    this._subscription.forEach((element) => {
+      element.unsubscribe();
+    });
   }
 
+  public logIn(): void {
+    const email: string = this.authorizationForm.controls['email'].value.trim();
+    const password: string = this.authorizationForm.controls['password'].value.trim();
+
+    this._subscription.push(this._authorizationService.logIn(email, password).subscribe(
+      (result) => {
+        if (!result) {
+          this.logInError = 'Ошибка авторизации';
+
+          return;
+        }
+
+        this.logInError = null;
+        
+        this._router.navigate(['/metering-devices']);
+      },
+      (error) => {
+        this.logInError = error?.error?.error?.data?.msg || 'Ошибка авторизации';
+
+        console.error(error);
+      },
+      () => { }
+    ));
+  }
 }
