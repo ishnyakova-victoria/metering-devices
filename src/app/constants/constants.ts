198e65142d3ba16bf117ceba32430e13f6b36db8
Victoria Ishnyakova <ishnyakova.vika@yandex.ru>
2021-02-26 23:39:07 +0700

    added constants

---
+++ src/app/constants/constants.ts
@@ -0,0 +1,4 @@
+export const URL_LOGIN: string = 'https://core.nekta.cloud/api/auth/login';
+export const URL_METERING_DEVICES: string = 'https://core.nekta.cloud/api/device/metering_devices';
+
+export const ACCESS_TOKEN: string = 'access_token';
