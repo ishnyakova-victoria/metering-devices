cc337147d344c9b8d8c42da14279fec3c821d109
Victoria Ishnyakova <ishnyakova.vika@yandex.ru>
2021-02-28 00:06:38 +0700

    added default request bodies

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,4 +1,25 @@
+import { AuthorizationRequestBody, MeteringDevicesRequestBody } from "../types/types";
+
 export const URL_LOGIN: string = 'https://core.nekta.cloud/api/auth/login';
 export const URL_METERING_DEVICES: string = 'https://core.nekta.cloud/api/device/metering_devices';
 
 export const ACCESS_TOKEN: string = 'access_token';
+
+export const DEFAULT_AUTHORIZATION_REQUEST_BODY: AuthorizationRequestBody = {
+    email: "string",
+    password: "string",
+    personal_data_access: true
+};
+
+export const DEFAULT_METERING_DEVICES_REQUEST_BODY: MeteringDevicesRequestBody = {
+    page: 1,
+    last_page: 0,
+    sort_field: "id",
+    sort: "desc",
+    search_string: null,
+    device_state: "all",
+    is_archived: false,
+    paginate: true,
+    append_fields: ["active_polling", "attributes", "tied_point"],
+    per_page: 10
+}
--- src/app/providers/api.service.ts
+++ src/app/providers/api.service.ts
@@ -1,8 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { URL_LOGIN } from '../constants/constants';
-import { AuthorizationRequestBody } from '../types/types';
+import { ACCESS_TOKEN, DEFAULT_AUTHORIZATION_REQUEST_BODY, DEFAULT_METERING_DEVICES_REQUEST_BODY, URL_LOGIN, URL_METERING_DEVICES } from '../constants/constants';
+import { AuthorizationRequestBody, MeteringDevicesRequestBody } from '../types/types';
+import { StorageService } from './storage.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,16 +11,30 @@ import { AuthorizationRequestBody } from '../types/types';
 export class ApiService {
   
   constructor(
-    private _httpClient: HttpClient
+    private _httpClient: HttpClient,
+    private _storageService: StorageService
   ) { }
 
   public logIn(email: string, password: string): Observable<any> {
     const body: AuthorizationRequestBody = {
-      email: email,
-      password: password,
-      personal_data_access: true
+      ...DEFAULT_AUTHORIZATION_REQUEST_BODY,
+      email,
+      password
     };
     
     return this._httpClient.post(URL_LOGIN, body);
+  }
+
+  public loadMeteringDevices(page: number, sortField: string, sort: string, perPage: number): Observable<any> {
+    const headers: HttpHeaders = new HttpHeaders({'Authorization': `Bearer ${this._storageService.getItem(ACCESS_TOKEN)}`});
+    const body: MeteringDevicesRequestBody = {
+      ...DEFAULT_METERING_DEVICES_REQUEST_BODY,
+      page,
+      sort,
+      sort_field: sortField,
+      per_page: perPage
+    };
+    
+    return this._httpClient.post(URL_METERING_DEVICES, body, {headers: headers});
   }
 }
